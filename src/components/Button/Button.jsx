20e20f64886842d9ce96fe39999ab2ed27c7fe40
Vitaly Nelyub <nelyub86@ukr.net>
2023-04-01 17:02:50 +0300

    fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,18 +3,6 @@ import css from '../Button/Button.module.css';
 // import { fetchImages } from '../Srvice/Fetch';
 
 class Button extends Component {
-  state = {};
-
-  // loadMore = () => {
-  //   const test = 'subaru';
-  //   // console.log(fetchImages(test))
-  //   console.log('MOREEEEE');
-  //   // console.log(fetchImages('subaru'));
-
-  //   // fetchImages(test).then(data => console.log(data));
-  // };
-
-  // fetchImages();
 
   render() {
     // console.log(this.props.currentValue);
